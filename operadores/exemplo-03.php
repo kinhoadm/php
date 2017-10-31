9b92dd20816abbf4fb0a6f619bad87485b19c349
Marcos Motta <kinhoadm@gmail.com>
2017-10-31 18:09:48 -0200

    atualizando...

--- operadores/exemplo-03.php
+++ operadores/exemplo-03.php
@@ -4,6 +4,34 @@
 
 // Operadores aritméticos:
 
+$a = 10;
+$b = 2;
 
+echo "A = " . $a . "; B = " . $b;
+echo "<br><br>";
+
+echo "Soma: ";
+echo $a + $b;
+echo "<br>";	
+
+echo "Subtração: ";
+echo $a - $b;
+echo "<br>";
+
+echo "Multiplicação: ";
+echo $a * $b;
+echo "<br>";
+
+echo "Divisão: ";
+echo $a / $b;
+echo "<br>";
+
+echo "Módulo: ";
+echo $a % $b;
+echo "<br>";
+
+echo "Exponenciação: ";
+echo $a ** $b;
+echo "<br>";
 
 ?>
